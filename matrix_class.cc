35a06625ecf06f4988f1f493125182f4eedf77ec
Tanmay Mandal <tmandal1988@gmail.com>
2021-07-05 15:59:54 -0700

    LU Decomposition is working

--- matrix_class.cc
+++ matrix_class.cc
@@ -207,7 +207,26 @@ Matrix<T> Matrix<T>::Inverse(){
 
 template<typename T>
 array<Matrix<T>, 2> Matrix<T>::Lu(){
-	array<Matrix, 2> lu_decomp;
+	if (nrows_ != ncols_)
+		throw invalid_argument("Matrix is not square\n");
+	array<Matrix<T>, 2> lu_decomp = {Matrix<T>(nrows_, ncols_, "eye"), Matrix<T>(nrows_, ncols_)};
+	T temp_sum;
+	for (size_t idx_c = 0; idx_c < ncols_; idx_c ++){
+		for (size_t idx_t = idx_c; idx_t < ncols_; idx_t++){
+			temp_sum = 0;
+			for (size_t idx_k = 0; idx_k < idx_c; idx_k++)
+				temp_sum += lu_decomp[0](idx_c, idx_k)*lu_decomp[1](idx_k, idx_t);
+
+			lu_decomp[1](idx_c, idx_t) = matrix_[idx_c][idx_t] - temp_sum;
+		}
+
+		for (size_t idx_t = (idx_c + 1); idx_t < ncols_; idx_t++){
+			temp_sum = 0;
+			for (size_t idx_k = 0; idx_k < idx_c; idx_k++)
+				temp_sum += lu_decomp[0](idx_t, idx_k) * lu_decomp[1](idx_k, idx_c);
+			lu_decomp[0](idx_t, idx_c) = (matrix_[idx_t][idx_c] - temp_sum)/lu_decomp[1](idx_c, idx_c);
+		}
+	}
 	return lu_decomp;
 }
 
@@ -391,8 +410,8 @@ int main()
 	m5(0, 2) = 5;
 
 	m5(1, 0) = 2;
-	m5(1, 1) = 10;
-	m5(1, 2) = 1;
+	m5(1, 1) = 20;
+	m5(1, 2) = 3;
 
 	m5(2, 0) = -1;
 	m5(2, 1) = 3;
@@ -402,6 +421,9 @@ int main()
 	array<Matrix<float>, 2> lu_decomp = m5.Lu();
 	lu_decomp[0].PrintMatrix();
 	lu_decomp[1].PrintMatrix();
+	m5 = lu_decomp[0]*lu_decomp[1];
+	m5.PrintMatrix();
+
 	m6.PrintMatrix();
 	m5(0, 0) = 0;
 	m5(1, 1) = 0;
